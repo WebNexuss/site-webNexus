87b867e8a4d907f697ee98c45ce08baf7e6ce48c
Jhonatan Silva <jhonatan.jsilvajos@gmail.com>
2025-03-08 13:31:24 -0300

    Criação das conversões

--- assets/js/scriptAds.js
+++ assets/js/scriptAds.js
@@ -1,17 +1,27 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
-    if(location.href.includes("contact")){
+    if (location.href.includes("contact")) {
 
         //Script para visualizar de página
+        gtag('event', 'conversion', {'send_to': 'AW-16915736770/ulO3CPPI4qcaEMLRhoI_'});
 
     }
 
-    document.addEventListener('click', function(e) {
-        if(e.target.href.includes("api.whatsapp")){
+
+    if (location.href.includes("/")) {
+
+        //Script para visualizar de página
+        gtag('event', 'conversion', { 'send_to': 'AW-16915736770/qEBaCImB8qcaEMLRhoI_' });
+
+    }
+
+    document.addEventListener('click', function (e) {
+        if (e.target.href.includes("api.whatsapp")) {
             // Acionamento WhatsApp
+            gtag('event', 'conversion', { 'send_to': 'AW-16915736770/qEBaCImB8qcaEMLRhoI_' });
         }
     })
 
-    
+
 
 })
